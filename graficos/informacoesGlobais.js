a60772e284f73bfca4d144cf36d5fc39a86e18e2
priscila a.g <108028422+priscilaandradegomes@users.noreply.github.com>
2024-10-25 20:57:35 +0000

    25/10/2024

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,9 +1,15 @@
-'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarinformacoeGlobais() {
-    const res =wait fetch(url)
-    const dados=await res.json()
+    const res = await fetch(url)
+    const dados= await res.json()
     console.log(dados);
-}
+    const paragrafo = document.createAttributeElement('p')
+    paragrafo.classlist.add ('graficos-conteiner_texto')
+    paragrafo.innerHTML = `você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas}estão total_pessoas_conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas total_pessoas_conectadas.`
+console.log(paragrafo)
 
-vizualizarinformacoeGlobais()
+const contrainer = document.getElementById('graficos-conteiner')
+contrainer.appendChild(paragrafo)
+}
+vizualizarinformacoeGlobais().catch
